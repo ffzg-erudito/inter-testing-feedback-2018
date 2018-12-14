8f8c2f4c7840f240707e903a02cadf7134294a07
denis <denis.vlasicek@gmail.com>
2018-12-14 12:57:33 +0100

    add makefile for importing text questions into oTree

---
+++ scripts/models_template.py
@@ -0,0 +1,276 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Matej Pavlić'
+
+doc = """
+This app is designed to investigate the forward testing effect
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'fwt'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+# Pitanja dodajemo tako da unesemo "label" kao prvi argument models.IntegerField-u!
+
+class Player(BasePlayer):
+    identity = models.StringField(label = 'prva dva slova imena oca, zadnja dva slova imena majke, i zadnja dva broja mobitela:')
+
+    def feedback(self, form_fields):
+        if form_fields == 2:
+            print('Točno')
+        else:
+            print('Netočno')
+
+    # practice questions
+    practice_q1 = models.IntegerField(label = 'Tlak kisika uvijek iznosi manje od:',
+            choices = [
+                    [1, '10% ukupnog atmosferskog tlaka'],
+                    [2, '21% ukupnog atmosferskog tlaka'],
+                    [3, '73% ukupnog atmosferskog tlaka'],
+                    [4, '87% ukupnog atmosferskog tlaka']],
+            widget = widgets.RadioSelect)
+    practice_q2 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    practice_q3 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+
+    # test1 content questions
+    test1_q1 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q2 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q3 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q4 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q5 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q6 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q7 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q8 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q9 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test1_q10 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+
+    # test2 content questions
+    test2_q1 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q2 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q3 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q4 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q5 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q6 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q7 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q8 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q9 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test2_q10 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+
+    # test3 content questions
+    test3_q1 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q2 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q3 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q4 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q5 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q6 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q7 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q8 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q9 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
+    test3_q10 = models.IntegerField(
+            choices = [
+                    [1, ''],
+                    [2, ''],
+                    [3, ''],
+                    [4, '']],
+            widget = widgets.RadioSelect)
---
+++ scripts/question-import.py
@@ -0,0 +1,72 @@
+import re
+import os
+
+# set paths
+modelsPath = 'models_template.py'
+
+questionsPath = os.path.join('questions',
+                             'pitanja-korovi-nepovezani-intruzori-py.txt')
+
+# read files
+with open(modelsPath, 'r') as infile:
+    modelScript = infile.readlines()
+
+with open(questionsPath, 'r') as infile:
+    items = infile.read()
+
+# clean questions and put them in a dict
+items = re.split('(?=\d\.\d{1,2}\.)', items)
+
+items = [item.strip() for item in items][1:]
+
+optSubtitution = {'a.': 1, 'b.': 2,  'c.': 3, 'd.': 4}
+questionImport = {}
+for item in items:
+    elements = re.split('\n +', item)
+    elements = [element.strip() for element in elements]
+
+    qnr, question = tuple(elements[0].split(' ', 1))
+
+    options = elements[1:]
+    options = [option.split(' ', 1) for option in options]
+    options = dict([[str(optSubtitution.get(option[0])), option[1]]
+                    for option in options])
+
+    questionImport.update({qnr: {'question': question,
+                                 'options': options}})
+
+outFile = open(os.path.join('exp-application', 'fwt_oTree', 'fwt',
+                            'out.py'), 'w')
+
+iModelScript = iter(modelScript)
+for line in iModelScript:
+    if re.search('\s+test\d_q\d{1,2}', line):
+        qKey = re.findall('test(\d)_q(\d{1,2})', line)
+        qKey = qKey[0][0] + '.' + qKey[0][1] + '.'
+
+        newLine = line +\
+        f"label = '{questionImport.get(qKey).get('question')}'" + '\n'
+        newLine = re.sub('\n', '', newLine, 1)
+        line = newLine
+
+        outFile.write(line)
+
+        # skip `choices= [` line
+        outFile.write(next(iModelScript))
+
+        # modify choice lines
+        for qNr in range(0,4):
+            line = next(iModelScript)
+
+            optNr = re.search('\d{1}', line).group(0)
+
+            line = re.sub("(?<=')(?=')",
+                        questionImport.get(qKey).get('options').get(str(optNr)),
+                        line)
+
+            outFile.write(line)
+
+    else:
+        outFile.write(line)
+
+outFile.close()
