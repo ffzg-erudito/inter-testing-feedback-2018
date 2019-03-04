ca9d4ae7f9860bcd6245d7a95c7a5e6755b2fa0a
denis <denis.vlasicek@gmail.com>
2019-03-04 21:08:09 +0100

    start wrangle, add data+dv2, rm instructions dir

--- analyses/wrangling/wrangling-main.R
+++ analyses/wrangling/wrangling-main.R
@@ -4,3 +4,22 @@ library(conflicted)
 library(magrittr)
 
 dat <- read_csv(here('data', 'results.csv'))
+
+# constructing intrusors DV as proportion of chosen intrusive distractors on
+# incorrectly answered questions
+
+getIntrusors <- function(x) {
+    x %<>% bind_rows(.)
+    propIntrusors <- x[which(x == 0)] %>% colnames(.) %>%
+        str_subset(., 'content') %>% str_extract(., '\\d{1,2}$') %>%
+        paste0('isIntrusor_3_', .) %>% {select(x, one_of(.))} %>%
+        rowMeans(.)
+
+    return(propIntrusors)
+}
+
+dat %>% select(., matches('content_3_|isIntrusor_3_')) %>%
+  apply(., getIntrusors, MARGIN = 1) -> dat$propIntrusors
+
+# dropping demographics and participant code
+dat %<>% select(., -c(participant_code, spol, dob))
