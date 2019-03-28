00a8dc745b2d451bc7144d03e95ee358c81a3d41
denis <denis.vlasicek@gmail.com>
2019-03-28 14:01:18 +0100

    1st manova

--- analyses/helpers/manova-helpers.R
+++ analyses/helpers/manova-helpers.R
@@ -3,4 +3,9 @@ covMatrixRatio <- function(rowi, coli, matrixList) {
   variances <- map_dbl(matrixList, function(x) {x[rowi, coli]})
   ratio <- max(variances) / min(variances)
   return(ratio)
-  }
+}
+
+omegaSquared <- function(n, lambda, dfh) {
+  omSq <- 1 - ((n * lambda) / (n - dfh - 1 + lambda))
+  return(omSq)
+}
