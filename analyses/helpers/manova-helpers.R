c378e2cc193009c66e77e5714a1c705b1a936588
denis <denis.vlasicek@gmail.com>
2019-04-15 12:52:54 +0200

    started 2nd manova

--- analyses/helpers/manova-helpers.R
+++ analyses/helpers/manova-helpers.R
@@ -9,3 +9,16 @@ omegaSquared <- function(n, lambda, dfh) {
   omSq <- 1 - ((n * lambda) / (n - dfh - 1 + lambda))
   return(omSq)
 }
+
+manovaStats <- function(manovaModel, statistic = 'test', effect,
+                        type = '3', test = 'Pillai') {
+  # specify effect as regex targeting the name of the effect in the `Anova`
+  # output table
+  Anova(manovaModel, type = type, test = test) %>% capture.output(.) %>%
+    str_subset(., effect) %>% str_extract_all(., '\\d+\\.?\\d*') %>%
+    pluck(1) -> results
+  
+  if (statistic == 'test') return(results[2])
+  else if (statistic == 'p') return(results[6])
+  else print('unrecognized statistic requested. try "p" or "test".')
+  }
